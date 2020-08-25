589869756cb85cda4f505b772849b8a82d602006
Andrew Park <atpark333@gmail.com>
2020-08-25 16:14:34 +0900

    Add migrations for account context

---
+++ priv/repo/migrations/20200825064238_create_users.exs
@@ -0,0 +1,17 @@
+defmodule SakeApp.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string
+      add :email, :string, null: false
+      add :password_hash, :string, null: false
+      add :birthdate, :date
+
+      timestamps()
+    end
+    
+    unique_index(:user, [:email])
+
+  end
+end
