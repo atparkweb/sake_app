81a7b008d20a69cc4352e97d5b28578e7604e1da
Andrew Park <atpark333@gmail.com>
2020-09-26 16:02:32 +0900

    Remove duplicate functions

--- lib/sake_app/guardian.ex
+++ lib/sake_app/guardian.ex
@@ -6,15 +6,9 @@ defmodule SakeApp.Guardian do
     sub = to_string(user.id)
     {:ok, sub}
   end
-  def subject_for_token(_, _) do
-    {:error, :reason_for_error}
-  end
   
   def resource_from_claims(claims) do
     user = claims["sub"] |> Accounts.get_user!
     {:ok, user}
-  end
-  def resource_from_claims(_claims) do
-    {:error, :reason_for_error}
   end
 end
