08cbbd94eef064b4749c740bcd2a5f204e73e1cf
Andrew Park <atpark333@gmail.com>
2020-09-26 15:46:23 +0900

    Add fallback functions for guardian token

--- lib/sake_app/guardian.ex
+++ lib/sake_app/guardian.ex
@@ -6,9 +6,15 @@ defmodule SakeApp.Guardian do
     sub = to_string(user.id)
     {:ok, sub}
   end
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
   
   def resource_from_claims(claims) do
     user = claims["sub"] |> Accounts.get_user!
     {:ok, user}
+  end
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
   end
 end
