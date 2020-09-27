b73435e56234a27b508737a5bc4b9af45219bdfd
Andrew Park <atpark333@gmail.com>
2020-09-27 18:28:34 +0900

    Add authorization pipeline for API and separate login path

--- lib/sake_app_web/router.ex
+++ lib/sake_app_web/router.ex
@@ -1,17 +1,30 @@
 defmodule SakeAppWeb.Router do
   use Phoenix.Router
-
+  
+  pipeline :protected do
+    plug CORSPlug, origin: "http://localhost:3000"
+    plug :accepts, ["json"]
+    plug :fetch_session
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.EnsureAuthenticated
+    plug Guardian.Plug.LoadResource
+  end
+  
   pipeline :api do
+    plug CORSPlug, origin: "http://localhost:3000"
     plug :accepts, ["json"]
     
-    forward "/api", Absinthe.Plug,
-      schema: SakeAppWeb.Schema
-
     forward "/graphiql", Absinthe.Plug.GraphiQL,
       schema: SakeAppWeb.Schema
+
+    plug Absinthe.Plug, schema: SakeAppWeb.Schema
+  end
+  
+  scope "/login", SakeAppWeb do
+    pipe_through :api
   end
 
-  scope "/", SakeAppWeb do
-    pipe_through :api
+  scope "/api/v1", SakeAppWeb do
+    pipe_through [:api, :protected]
   end
 end
